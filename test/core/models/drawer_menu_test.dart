ecee6cc4c01f6ba1e531b01c3547e8ad08b88af6
Alann Maulana <alann_it06@yahoo.co.id>
2019-12-31 13:33:01 +0700

    init unit test

---
+++ test/core/models/drawer_menu_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../lib/core/models/drawer_menu.dart';
+
+void main() {
+  test('Check constant value if equal', () {
+    expect(DrawerMenu.browser.label == 'Browser', isTrue);
+    expect(DrawerMenu.info.label == 'Info', isTrue);
+    expect(DrawerMenu.serverInfo.label == 'Server Info', isTrue);
+    expect(DrawerMenu.serverInfo.parent == DrawerMenu.info, isTrue);
+  });
+
+  test('Check created instance value if equal', () {
+    const test = DrawerMenu('Test', DrawerMenu.browser, 'value-test');
+
+    expect(test.label == 'Test', isTrue);
+    expect(test.parent == DrawerMenu.browser, isTrue);
+    expect(test.value == 'value-test', isTrue);
+  });
+
+  test('Check if created instance defined constant if equal', () {
+    const test = const DrawerMenu('Browser');
+
+    expect(test == DrawerMenu.browser, isTrue);
+    expect(test.label == DrawerMenu.browser.label, isTrue);
+    expect(test.parent == DrawerMenu.browser.parent, isTrue);
+    expect(test.value == DrawerMenu.browser.value, isTrue);
+  });
+}
