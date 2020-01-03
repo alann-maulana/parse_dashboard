043069a4f3234f5e7465eeb9f8269607a31dc7ca
Alann Maulana <alann_it06@yahoo.co.id>
2020-01-03 10:12:59 +0700

    adding sentry reporting at release

---
+++ lib/core/platform/platform_html.dart
@@ -0,0 +1,54 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+import 'package:platform/platform.dart';
+
+class TargetPlatform extends Platform {
+  @override
+  Map<String, String> get environment => {};
+
+  @override
+  String get executable => '';
+
+  @override
+  List<String> get executableArguments => [];
+
+  @override
+  String get localHostname => html.window.navigator.appName;
+
+  @override
+  String get localeName => '';
+
+  @override
+  int get numberOfProcessors => 0;
+
+  @override
+  String get operatingSystem => html.window.navigator.platform;
+
+  @override
+  String get operatingSystemVersion => '';
+
+  @override
+  String get packageConfig => '';
+
+  @override
+  String get packageRoot => '';
+
+  @override
+  String get pathSeparator => '/';
+
+  @override
+  String get resolvedExecutable => '';
+
+  @override
+  Uri get script => Uri();
+
+  @override
+  bool get stdinSupportsAnsi => false;
+
+  @override
+  bool get stdoutSupportsAnsi => false;
+
+  @override
+  String get version => html.window.navigator.appVersion;
+}
